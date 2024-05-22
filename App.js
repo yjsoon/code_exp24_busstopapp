3c94f1abad3bee8c5a2481115b3bb830f15be0c3
YJ Soon <yjsoon@tinkertanker.com>
2024-05-22 15:11:43 +0800

    All done oops

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  ActivityIndicator
+} from "react-native";
+import { useEffect, useState } from "react";
+
+const API_URL = "https://arrivelah2.busrouter.sg/?id=84221";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [timing, setTiming] = useState("");
+
+  function fetchData() {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        const myBus = data.services.filter((bus) => bus.no === "14")[0];
+        console.log(myBus);
+        setTiming(myBus.next.time);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator size="large" /> : timing}
+      </Text>
+      <Pressable style={styles.button} onPress={fetchData}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +44,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20
   },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  arrivalTime: {
+    fontSize: 40,
+    fontWeight: "bold"
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5
+  },
+  buttonText: {
+    color: "white"
+  }
 });
